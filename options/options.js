47decb3566690f82b9756b1862a6bd243910fb8d
Crowdbotics <engineering@crowdbotics.com>
2022-12-09 16:45:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tst-daniel-databind-49740",
+  url: "https://tst-daniel-databind-49740.botics.co",
+  api: "https://tst-daniel-databind-49740.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
